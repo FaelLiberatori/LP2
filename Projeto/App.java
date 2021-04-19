fa48f89d141a60dfd086271efd2e68272a764388
FaelLiberatori <rafaelliberatori@outlook.com>
2021-04-19 01:36:55 -0300

    Movido projeto para um JPanel, a fim de evitar flick.

--- Projeto/App.java
+++ Projeto/App.java
@@ -6,119 +6,127 @@ import java.util.Random;
 
 import figures.*;
 
-public class App {
-    public static void main (String[] args) {
-        ListFrame frame = new ListFrame();
-        frame.setVisible(true);
+public class App extends JFrame {
+
+    App() {
+        super("Projeto Vetorial");
+        MainPanel panel = new MainPanel();
+        panel.setFocusable(true);
+        this.add(panel);
+        this.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        this.setLocationByPlatform(true);
+        this.pack();
+        this.setVisible(true);
     }
-}
 
-class ListFrame extends JFrame {
-    ArrayList<Figure> figList = new ArrayList<Figure>();
-    Random rand = new Random();
-    Figure activeFigure = null;
-    int mousePositionX = -100, mousePositionY = -100;
+    class MainPanel extends JPanel {
+        ArrayList<Figure> figList = new ArrayList<Figure>();
+        Random rand = new Random();
+        Figure activeFigure = null;
+        int mousePositionX = -100, mousePositionY = -100;
 
-    ListFrame () {
-        this.addWindowListener (
-            new WindowAdapter() {
-                public void windowClosing (WindowEvent e) {
-                    System.exit(0);
-                }
-            }
-        );
+        MainPanel() {
+            setPreferredSize(new Dimension(1024, 720));
+            setBackground(Color.WHITE);
+            setDoubleBuffered(true);
 
-        this.addMouseMotionListener (
-            new MouseMotionAdapter() {
-                public void mouseMoved (MouseEvent evt) {
-                    mousePositionX = evt.getX();
-                    mousePositionY = evt.getY();
-                }
-
-                public void mouseDragged (MouseEvent evt) {
-                    if (activeFigure != null) {
-                        activeFigure.drag(evt.getX(), evt.getY());
-                        repaint();
+            this.addMouseMotionListener (
+                new MouseMotionAdapter() {
+                    public void mouseMoved (MouseEvent evt) {
+                        mousePositionX = evt.getX();
+                        mousePositionY = evt.getY();
                     }
-                }
-            }
-        );
-
-        this.addMouseListener(
-            new MouseAdapter() {
-                public void mousePressed(MouseEvent evt) {
-                    if (activeFigure != null) {
-                        activeFigure.isSelected = false;
-                        activeFigure = null;
-                    }
-
-                    for (Figure fig: figList) {
-                        if (fig.isOnPoint(mousePositionX, mousePositionY))
-                            activeFigure = fig;
-                    }
-
-                    if (activeFigure != null) {
-                        figList.add(figList.remove(figList.indexOf(activeFigure)));
-                        activeFigure = figList.get(figList.size() -1);
-                        activeFigure.isSelected = true;
-                    }
-                    
-                    repaint();
-                }
-            }
-        );
-
-        this.addKeyListener (
-            new KeyAdapter() {
-                public void keyPressed (KeyEvent evt) {
-                    int x = mousePositionX;
-                    int y = mousePositionY;
-                    int w = 40;
-                    int h = 40;
-                    int b = 5;
-                    int a = 0;
-
-                    int key = evt.getKeyCode();
-
-                    if (key == evt.VK_R) {
-                        figList.add(new Rect(x, y, w, h, b, a, Color.yellow, Color.black));
-                        repaint();
-                    }
-                    if (key == evt.VK_E) {
-                        figList.add(new Ellipse(x, y, w, h, b, a, Color.yellow, Color.black));
-                        repaint();
-                    }
-                    if (key == evt.VK_T) {
-                        figList.add(new Triangle(x, y, w, h, b, a, Color.yellow, Color.black));
-                        repaint();
-                    }
-                    if (key == evt.VK_Y) {
-                        figList.add(new Parallelogram(x, y, w, h, b, a, Color.yellow, Color.black));
-                        repaint();
-                    }
-                    if (key == evt.VK_DELETE) {
+    
+                    public void mouseDragged (MouseEvent evt) {
                         if (activeFigure != null) {
-                            figList.remove(figList.size() - 1);
-                            figList.remove(activeFigure);
-                            activeFigure = null;
+                            activeFigure.drag(evt.getX(), evt.getY());
                             repaint();
                         }
                     }
                 }
-            }
-        );
+            );
 
-        this.setTitle("Projeto Vetorial");
-        this.setSize(1024, 768);
+            this.addMouseListener(
+                new MouseAdapter() {
+                    public void mousePressed(MouseEvent evt) {
+                        if (activeFigure != null) {
+                            activeFigure.isSelected = false;
+                            activeFigure = null;
+                        }
+    
+                        for (Figure fig: figList) {
+                            if (fig.isOnPoint(mousePositionX, mousePositionY))
+                                activeFigure = fig;
+                        }
+    
+                        if (activeFigure != null) {
+                            figList.add(figList.remove(figList.indexOf(activeFigure)));
+                            activeFigure = figList.get(figList.size() -1);
+                            activeFigure.isSelected = true;
+                        }
+                        
+                        repaint();
+                    }
+                }
+            );
+    
+            this.addKeyListener (
+                new KeyAdapter() {
+                    public void keyPressed (KeyEvent evt) {
+                        int x = mousePositionX;
+                        int y = mousePositionY;
+                        int w = 40;
+                        int h = 40;
+                        int b = 5;
+                        int a = 0;
+    
+                        int key = evt.getKeyCode();
+    
+                        if (key == evt.VK_R) {
+                            figList.add(new Rect(x, y, w, h, b, a, Color.yellow, Color.black));
+                            repaint();
+                        }
+                        if (key == evt.VK_E) {
+                            figList.add(new Ellipse(x, y, w, h, b, a, Color.yellow, Color.black));
+                            repaint();
+                        }
+                        if (key == evt.VK_T) {
+                            figList.add(new Triangle(x, y, w, h, b, a, Color.yellow, Color.black));
+                            repaint();
+                        }
+                        if (key == evt.VK_Y) {
+                            figList.add(new Parallelogram(x, y, w, h, b, a, Color.yellow, Color.black));
+                            repaint();
+                        }
+                        if (key == evt.VK_DELETE) {
+                            if (activeFigure != null) {
+                                figList.remove(figList.size() - 1);
+                                figList.remove(activeFigure);
+                                activeFigure = null;
+                                repaint();
+                            }
+                        }
+                    }
+                }
+            );
+        }
+
+        @Override
+        public void paintComponent (Graphics g) {
+            super.paintComponent(g);
+    
+            for (Figure fig: figList) {
+                fig.paint(g);
+            }
+        }
     }
 
-    public void paint (Graphics g) {
-        super.paint(g);
-        g.setColor(Color.white);
-        g.fillRect(0, 0, getWidth(), getHeight());
-
-        for (Figure fig: figList) {
-            fig.paint(g);
-        }
+    public static void main(String[] args) {
+        EventQueue.invokeLater(new Runnable() {
+            @Override
+            public void run() {
+                App app = new App();
+            }
+        });
     }
 }
